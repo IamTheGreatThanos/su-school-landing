3f83933678cab16af6de6d7059095fdef93f33f3
999 <iamthegreatthanos@gmail.com>
2024-08-14 00:53:34 +0500

    Added other sections.

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
-function open_menu(){
+function open_menu() {
     let menu_mobile_block = document.querySelector('.menu__mobile_block')
     let menu_mobile = document.querySelector('.menu__mobile')
     menu_mobile.style.transform = 'translateX(0)'
-    menu_mobile_block.style.display='block'
+    menu_mobile_block.style.display = 'block'
 }
-function close_menu($event){
+function close_menu($event) {
     let el = $event.srcElement
     console.log(el);
     if (el.classList.contains('menu__mobile_block')) {
